ca22d27a4c8c54f3b632f74aaa9e078711ce7fa6
Mihail93A <strannodan@mail.ru>
2024-02-13 00:50:53 +0500

    2

---
+++ src/main/java/Main.java
@@ -0,0 +1,15 @@
+public class Main {
+
+    public static void main(String[] args ) {
+
+Transport bmw = new Transport();
+bmw. speed = 250.5F;
+bmw.weight = 2500;
+bmw.color = "White";
+bmw.coordinate = new byte [] {0, 0, 0};
+
+Transport truck = new Transport();
+
+    }
+
+}
---
+++ src/main/java/Transport.java
@@ -0,0 +1,11 @@
+public class Transport {
+
+public float speed;
+public int weight;
+public String color;
+public byte [] coordinate;
+
+
+
+
+}
